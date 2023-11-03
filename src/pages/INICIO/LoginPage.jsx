1d83fd88f820daf678c55cfaa36e9b71612b3b56
kevin <acosta.kevin@correounivalle.edu.co>
2023-11-03 15:02:57 -0500

    commit antes de pullear las cosas de actualizacion de informacion de usuario

--- src/pages/INICIO/LoginPage.jsx
+++ src/pages/INICIO/LoginPage.jsx
@@ -18,9 +18,11 @@ export default function LoginPage() {
         correo: email,
         password: password,
       });
-      console.log("Response: ", response);
+      //console.log("Response: ", response);
       // Aquí es donde se recibe el token
       const token = response.data.token;
+      const username = response.data.username;
+     // console.log(username);
       // Ahora puedes almacenar el token en el almacenamiento local para su uso futuro
       localStorage.setItem("token", token);
 
@@ -28,22 +30,22 @@ export default function LoginPage() {
       //this.props.history.push("/Contenido"); // Si estás usando react-router
       navigate("/Contenido");
     } catch (error) {
-      console.log("Error: ", error);
+      //console.log("Error: ", error);
       // Manejar errores
       if (error.response) {
         // La solicitud se realizó y el servidor respondió con un código de estado
         // que cae fuera del rango de 2xx
-        console.log(error.response.data);
+        /**console.log(error.response.data);
         console.log(error.response.status);
-        console.log(error.response.headers);
+        console.log(error.response.headers); */
         setError(error.response.data.error);
       } else if (error.request) {
         // La solicitud se realizó pero no se recibió ninguna respuesta
-        console.log(error.request);
+        //console.log(error.request);
         setError("No se recibió ninguna respuesta del servidor.");
       } else {
         // Algo sucedió en la configuración de la solicitud que provocó un error
-        console.log("Error", error.message);
+        //console.log("Error", error.message);
         setError(error.message);
       }
     }
