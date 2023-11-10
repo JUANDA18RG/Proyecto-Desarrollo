6a7d3b5c30b69c838410b5caff52c3b2cb1feb5e
JUAN DAVID RAMIREZ GRISMALDO <105172937+JUANDA18RG@users.noreply.github.com>
2023-11-09 22:27:53 -0500

    version JuanDa

--- src/pages/LIBROS/Books.jsx
+++ src/pages/LIBROS/Books.jsx
@@ -36,20 +36,27 @@ const BookList = () => {
               books.map((libro) => (
                 <div
                   key={libro.ISBN}
-                  className="bg-white rounded-lg overflow-hidden shadow-md transform transition-transform hover:scale-105 relative"
+                  className="bg-white rounded-lg overflow-hidden shadow-md transform transition-transform hover:scale-105 m-4"
                 >
                   <Link to={`/book/${libro.ISBN}`} className="block">
                     <img
                       src={`http://localhost:4000${libro.portada}`}
                       alt={libro.titulo}
-                      className="w-full h-64 object-cover"
+                      className="w-full h-96 object-cover rounded-t-md"
                     />
                     <div className="p-4">
-                      <h2 className="text-xl font-semibold">{libro.titulo}</h2>
-                      <p className="text-gray-600">Author: {libro.autor}</p>
+                      <h2 className="text-xl font-semibold text-center mb-4">
+                        {libro.titulo}
+                      </h2>
+                      <p className="text-gray-600 m-1 text-center">
+                        Author: {libro.autor}
+                      </p>
+                      <p className="text-gray-600 m-1 text-center">
+                        Categoria: {libro.genero}
+                      </p>
                     </div>
-                    <div className="text-gray-700 flex text-right">
-                      <div className="flex items-center bg-green-400 m-2 rounded">
+                    <div className="text-gray-700 flex p-3">
+                      <div className="flex items-center bg-green-400 rounded m-2">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -65,10 +72,10 @@ const BookList = () => {
                           />
                         </svg>
                         <p className="m-1 text-black">
-                          {libro.copiasDisponibles}
+                          Disponibles: {libro.copiasDisponibles}
                         </p>
                       </div>
-                      <div className="flex items-center m-2 bg-red-400 rounded">
+                      <div className="flex items-center bg-red-400 rounded m-2">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -84,34 +91,32 @@ const BookList = () => {
                           />
                         </svg>
                         <p className="m-1 text-black">
-                          {libro.copiasReservadas}
+                          Reservados: {libro.copiasReservadas}
                         </p>
                       </div>
                     </div>
                   </Link>
-                  <div className="flex">
-                    <div className="absolute bottom-0 right-2 flex items-center">
-                      <span className="text-yellow-500">
-                        {libro.valoracion}
-                      </span>
-                      <span className="p-2 bg-white rounded-full">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="currentColor"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="transparent"
-                          className="w-8 h-8 text-yellow-500"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                            fill="yellow"
-                          />
-                        </svg>
-                      </span>
-                    </div>
+                  <div className="flex items-center justify-center m-2">
+                    <span className="text-yellow-500 text-lg">
+                      Valoración: {libro.valoracion}
+                    </span>
+                    <span className="p-2 bg-white rounded-full ml-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="transparent"
+                        className="w-10 h-10 text-yellow-500 animate-pulse"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                          fill="yellow"
+                        />
+                      </svg>
+                    </span>
                   </div>
                 </div>
               ))
