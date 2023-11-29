cfabc4d92ec6d15e1fda808d77f37f3bee3cd028
Niko <nicolas.arturo.valencia@correounivalle.edu.co>
2023-11-28 19:03:57 -0500

    santiago

--- src/pages/LIBROS/Contenido.jsx
+++ src/pages/LIBROS/Contenido.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './Header';
 import InfoLibro from './InfoLibro';
 import Inicio from './Inicio';
@@ -7,33 +8,31 @@ import Chat from './Help';
 import Footer from './Footer';
 import Busqueda from './Busqueda';
 
-
 export default function Contenido() {
-  const handleBuscarLibro = (Busqueda, filtro) => {
-    console.log(Busqueda, filtro);
-  }
- 
+  const handleBuscarLibro = (busqueda, filtro, disponibilidadFiltro) => {
+    console.log('Búsqueda:', busqueda);
+    console.log('Filtro:', filtro);
+    console.log('Disponibilidad Filtro:', disponibilidadFiltro);
+  };
+
   return (
     <>
-    {/*-----------------Header-----------------*/}
-    <Header/>
-    {/*-----------------Bienvenida a el usuario-----------------*/}
-    <Inicio/>
- 
-    {/*-----------------Carruseles-----------------*/}
-    <Carruseles/>
-    {/*-----------------Busqueda-----------------*/}
-    <Busqueda onBuscar={handleBuscarLibro}/>
-    {/*-----------------Libros mas reservados-----------------*/}
-    <Books/>
-    {/*-----------------Info sobre los libros-----------------*/}
-    <InfoLibro/>
-    {/*-----------------Chat-----------------*/}
-    <Chat/>
-    {/*-----------------Footer-----------------*/}
-    <Footer/>
+      {/*-----------------Header-----------------*/}
+      <Header />
+      {/*-----------------Bienvenida a el usuario-----------------*/}
+      <Inicio />
+      {/*-----------------Carruseles-----------------*/}
+      <Carruseles />
+      {/*-----------------Busqueda-----------------*/}
+      <Busqueda onBuscar={handleBuscarLibro} />
+      {/*-----------------Libros mas reservados-----------------*/}
+      <Books />
+      {/*-----------------Info sobre los libros-----------------*/}
+      <InfoLibro />
+      {/*-----------------Chat-----------------*/}
+      <Chat />
+      {/*-----------------Footer-----------------*/}
+      <Footer />
     </>
-    
- 
-);
-} 
+  );
+}
