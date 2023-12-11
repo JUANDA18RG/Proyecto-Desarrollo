7e58f1096a38c37e4257c5979178260f5326dbd8
Andrea Cifuentes <cifuentes17andrea@gmail.com>
2023-12-11 16:32:25 -0500

    Editar libro terminado

--- src/pages/ADMIN/EditarLibro.jsx
+++ src/pages/ADMIN/EditarLibro.jsx
@@ -4,21 +4,46 @@ import axios from "axios";
 import { RiSearchLine } from 'react-icons/ri';
 import Swal from "sweetalert2";
 
-{/*Falta poder editar la imagen */}
 
 const EditarLibro = () => {
+
+  const handleRemoveImage = () => {
+    setImagenPortada(null);
+    setPreviewImagen(null);
+  };
+    const [imagenPortada, setImagenPortada] = useState(null);
+    const [previewImagen, setPreviewImagen] = useState(null);
     const [books, setBooks] = useState ([]);
     const [loading, setLoading] = useState(true);
-    const [editFormData, setEditFormData] = useState({});
     const [showEditForm, setShowEditForm] = useState(false);
     const [busqueda, setBusqueda] = useState('');  
     const navigate = useNavigate();
-
-const goToInicio = () => {
+    const [editFormData, setEditFormData] = useState({
+    isbn: "",
+    titulo: "",
+    autor: "",
+    genero: "",
+    cantcopias: "",
+    aniopublicacion: "",
+    sinopsis: "",
+  });
+      const goToInicio = () => {
         navigate("/ContenidoAdmin/false");
       }; 
     
+      const handleImagenChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+          setImagenPortada(file);
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            setPreviewImagen(reader.result);
+          };
+          reader.readAsDataURL(file);
+        }
+      };
       
+
 const cargarLibros = async () => {
         try {
           setLoading(true);
@@ -36,27 +61,22 @@ useEffect(() => {
 }, []);
    
 const handleEditClick = (libro) => {
-        console.log('Libro details:', libro);
-        setEditFormData({
-          ...libro,
-        });
-        setShowEditForm(true);
-      };
-      
-      
-const handleEditFormChange = (e) => { 
-  if (e.target.name === 'portada') {
-    setEditFormData({
-          ...editFormData,
-          portada: e.target.files[0],
-          
-        })
-      }else{
-        setEditFormData({
-          ...editFormData,
-        [e.target.name]: e.target.value,     
-             })};
+  console.log('Libro details:', libro);
+  setEditFormData({
+    ...libro,
+  });
+  setShowEditForm(true);
 };
+      
+    
+const handleEditFormChange = (e) => {
+  const { name, value } = e.target;
+  setEditFormData((prevLibro) => ({
+    ...prevLibro,
+    [name]: value,
+  }));
+};
+
 
 
 const handleEditFormSubmit = async () => {
@@ -68,34 +88,60 @@ const handleEditFormSubmit = async () => {
       console.error("Token no disponible");
       return;
     }
-
-    const headers = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      
-      },
-    };
+   
+    const formData = new FormData();
+    Object.keys(editFormData).forEach((key) => formData.append(key, editFormData[key]));
+   
+    if (imagenPortada) {
+      formData.append("file", imagenPortada);
+    }
     
-    const response = await axios.put(`http://localhost:4000/libros/${isbn}`, editFormData, headers);
 
-    if (response.status === 200) {
+    
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+    const res = await axios.put(
+      `http://localhost:4000/updateLibro/${isbn}`, 
+      formData, 
+      config
+    );
+
+    if (res.status === 200) {
       Swal.fire({
-        icon: 'success',
-        title: 'Actualización exitosa',
-        text: 'La información del libro ha sido actualizada correctamente.',
+        icon: "success",
+        title: "Libro editado",
+        showConfirmButton: false,
+        timer: 1500,
       });
-      cargarLibros();
+   
+      cargarLibros(); 
     } else {
-      console.error("Error al actualizar la información del libro. Estado:", response.status);
+      console.error("Error al actualizar la información del libro. Estado:", res.status, res.data);
+      res.status(500).json({ error: 'Error interno del servidor' });
     }
   } catch (error) {
-    console.error("Error durante la actualización:", error);
+    console.error("Error en el servidor:", error);
+   
+    if (error.response) {
+      if (error.response.status === 400 && error.response.data.error) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: error.response.data.error,
+        });
 
-    Swal.fire({
-      icon: 'error',
-      title: 'Error',
-      text: 'Hubo un error al actualizar la información del libro.',
-    });
+      }else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Hubo un error al actualizar la información del libro.',
+      });
+    }
+    }
   } finally {
     setShowEditForm(false);
   }
@@ -301,14 +347,13 @@ return (
               </div>
            
 
-  {console.log('showEditForm:', showEditForm)}
     {showEditForm && (
         <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center z-20">
          <div className="bg-white p-8 rounded-md flex flex-col items-center">
             <h2 className="text-2xl font-semibold mb-4 text-center">Editar el libro "{editFormData.isbn}"</h2>
-            
-            <form>
-            <div className="flex space-x-20">
+          
+          <div className="flex">
+          <form className="flex flex-col items-center justify-center m-4">
             <div className="w-2/3 flex flex-col space-x-4">
             <label>
               <input
@@ -316,7 +361,7 @@ return (
                 name="isbn"
                 value={editFormData.isbn || ''}
                 onChange={handleEditFormChange}
-                className="border p-2 w-full"
+               
               />
             </label>
             <label>
@@ -326,7 +371,7 @@ return (
                   name="titulo"
                   value={editFormData.titulo || ''}
                   onChange={handleEditFormChange}
-                  className="border p-2 w-full"
+                  className="border-2 border-gray-400 p-2 rounded-lg m-2 w-80 focus:outline-none focus:border-pink-500 transition duration-300 ease-in-out focus:ring-2 focus:ring-pink-500"
                 />
               </label>
               <label>
@@ -336,7 +381,7 @@ return (
                   name="autor"
                   value={editFormData.autor || ''}
                   onChange={handleEditFormChange}
-                  className="border p-2 w-full"
+                  className="border-2 border-gray-400 p-2 rounded-lg m-2 w-80 focus:outline-none focus:border-pink-500 transition duration-300 ease-in-out focus:ring-2 focus:ring-pink-500"
                 />
               </label>
               <label>
@@ -346,80 +391,143 @@ return (
                   name="genero"
                   value={editFormData.genero || ''}
                   onChange={handleEditFormChange}
-                  className="border p-2 w-full"            
+                  className="border-2 border-gray-400 p-2 rounded-lg m-2 w-80 focus:outline-none focus:border-pink-500 transition duration-300 ease-in-out focus:ring-2 focus:ring-pink-500"     
                 />
               </label>
-              <label>
-              <span className="text-gray-700 font-semibold">Año publicacion:</span>
-                <input
-                  type="text"
-                  name="aniopublicacion"
-                  value={editFormData.aniopublicacion || ''}
-                  onChange={handleEditFormChange}
-                  className="border p-2 w-full"             
-                />
-              </label>
+              <span className="text-gray-700 font-semibold">Año publicación:</span>
+              <select
+                onChange={handleEditFormChange}
+                name="aniopublicacion"
+                value={editFormData.aniopublicacion}
+                required
+                className="border-2 border-gray-400 p-2 rounded-lg m-2 w-80 focus:outline-none focus:border-pink-500 transition duration-300 ease-in-out focus:ring-2 focus:ring-pink-500"
+              >
+                {// Genera los años desde 1400 hasta el año actual
+                Array.from(
+                  { length: new Date().getFullYear() - 1400 + 1 },
+                  (_, i) => 1400 + i
+                ).map((year) => (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
+                ))}
+              </select>
               <label>
               <span className="text-gray-700 font-semibold">Cantidad de copias:</span>
-                <input
-                  type="text"
-                  name="cantcopias"
-                  value={editFormData.cantcopias || ''}
-                  onChange={handleEditFormChange}
-                  className="border p-2 w-full"
-                />
+              <input
+                onChange={handleEditFormChange}
+                type="number"
+                name="cantcopias"
+                value={editFormData.cantcopias}
+                min="0"
+                max="infinito"
+                className="border-2 border-gray-400 p-2 rounded-lg m-2 w-80 focus:outline-none focus:border-pink-500 transition duration-300 ease-in-out focus:ring-2 focus:ring-pink-500"
+              />
               </label>
               <label>
-              <span className="text-gray-700 font-semibold">Sinopsis:</span>
+              <span className="text-gray-700 font-semibold">Descripción:</span>
                 <textarea
                   name="sinopsis"
                   value={editFormData.sinopsis || ''}
                   onChange={handleEditFormChange}
-                  className="border p-2 w-full"
+                  className="border-2 border-gray-400 p-2 rounded-lg m-2 w-80 focus:outline-none focus:border-pink-500 transition duration-300 ease-in-out focus:ring-2 focus:ring-pink-500"
                   rows={calculateTextareaRows(editFormData.sinopsis)}
                 />
               </label>
               </div>
-              <div className="w-2/3 flex items-center justify-center space-x-4">
-              <label className='mb-4'>
-              <span className="text-gray-700 font-semibold">Cambiar imagen:</span>
-                <input
-                  type="file"
-                  accept="image/*"
-                  name = "portada"
-                  onChange={handleEditFormChange}
-                  className="border p-24"
-                />
-              </label>
+              </form>
+              
+              <div className="mt-10 p-6 ml-6 text-center z-10 m-5">
+              <div className="flex flex-col items-center justify-center"></div>
+              <form className="text-center mx-auto w-80">
+              <div className="border-2 border-gray-400 p-2 rounded-lg m-2 relative flex items-center justify-center h-96 hover:border-pink-600 transition duration-300 ease-out">
+              <input
+                      onChange={handleImagenChange}
+                      type="file"
+                      name="file"
+                      id="imagenPortada"
+                      accept="image/*"
+                      required
+                      className="absolute opacity-0 w-full h-full cursor-pointer"
+                    />
+                    {previewImagen ? (
+                      <div className="relative">
+                        <button
+                          onClick={handleRemoveImage}
+                          className="absolute top-2 right-2 bg-red-500 text-white p-1 rounded-full shadow-md hover:bg-red-600 transition duration-300 ease-in-out"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M6 18L18 6M6 6l12 12"
+                            />
+                          </svg>
+                        </button>
+                        <img
+                          src={previewImagen}
+                          alt="imagen"
+                          className="w-full h-full object-cover  rounded-lg"
+                        />
+                      </div>
+                    ) : (
+                      <div className="flex flex-col items-center justify-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-16 w-16 text-gray-400"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={0.5}
+                            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                          />
+                        </svg>
+                        <p className="text-gray-400">Sube una imagen</p>
+                      </div>
+                    )}
+                  </div>
+             
+              </form>
               </div>
               </div>
              
-              <div className="flex items-center justify-center mt-4 space-x-28">
+             <div className='space-x-36'>
               <button
                 type="button"
                 onClick={handleEditFormSubmit}
                 className="bg-pink-500 text-white py-2 px-4 rounded-md"
               >
-               Save change
+              Guardar cambios
               </button>
               <button
                 type="button"
                 onClick={() => setShowEditForm(false)}
                 className="bg-pink-500 text-white py-2 px-4 rounded-md"
               >
-              Go out
+              Salir
               </button>
               </div>
-            </form>
-          </div>
+          
+              </div>     
         </div>
-      )}
+        )}
 </div>    
 );
 }
 
 
-
-
 export default EditarLibro;
+
+
 
